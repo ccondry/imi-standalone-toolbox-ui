645eab58fe5337721fb16f2e5c35cd966e38ab6a
Coty Condry <ccondry@cisco.com>
2022-01-12 12:09:44 -0600

    Revert "remove unused input fields, combine currency and debt amount"
    This reverts commit 71181a73bb0c63d98bdacc996802c531d868f9c6.

--- src/components/collections-demo.vue
+++ src/components/collections-demo.vue
@@ -69,12 +69,52 @@
       </b-field>
     </b-field>
 
+    <!-- customer email -->
+    <b-field label="Email">
+      <b-input v-model="form.customerEmail" />
+    </b-field>
+
     <!-- country and currency -->
     <b-field grouped>
+      <!-- currency -->
+      <b-field label="Currency">
+        <!-- $ -->
+        <b-radio-button
+        v-model="form.currency"
+        native-value="$"
+        type="is-success is-light is-outlined"
+        >
+          <span>$</span>
+        </b-radio-button>
+        
+        <!-- £ -->
+        <b-radio-button
+        v-model="form.currency"
+        native-value="£"
+        type="is-success is-light is-outlined"
+        >
+          <span>£</span>
+        </b-radio-button>
+
+        <!-- € -->
+        <b-radio-button
+        v-model="form.currency"
+        native-value="€"
+        type="is-success is-light is-outlined"
+        >
+          <span>€</span>
+        </b-radio-button>
+      </b-field>
+
       <!-- debt amount -->
       <b-field label="Debt Amount">
         <b-input v-model="form.debtAmount" required />
       </b-field>
+    </b-field>
+
+    <!-- customer reference number -->
+    <b-field label="Reference Number">
+      <b-input v-model="form.customerReferenceNumber" />
     </b-field>
 
     <!-- send button -->
@@ -100,10 +140,13 @@ export default {
         country: 'US',
         customerName: '',
         customerNumber: '',
+        customerEmail: '',
         customerSalutation: 'Mx',
-        debtAmount: '$200',
+        debtAmount: '200',
+        currency: '$',
         reminderRequiredStatus: 'Y',
-        channel: 'sms'
+        channel: 'sms',
+        customerReferenceNumber: '0012E00002AHLDEQA5'
       }
     }
   },
