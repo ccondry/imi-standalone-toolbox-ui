f34d1095bfc4a97a0449734ed687d1edfaa2be13
Coty Condry <ccondry@cisco.com>
2022-01-14 11:32:36 -0600

    add call deflection demo panel

---
+++ src/components/call-deflection-demo.vue
@@ -0,0 +1,12 @@
+<template>
+  <panel title="Call Deflection" aria-id="call-deflection">
+    <p>
+      Call this number to start the Call Deflection Demo.
+    </p>
+    <p>
+      <strong>
+        +1 206-848-7318
+      </strong>
+    </p>
+  </panel>
+</template>
--- src/components/demo.vue
+++ src/components/demo.vue
@@ -2,18 +2,21 @@
   <panel title="Demos" aria-id="demos">
     <div style="display: flex; justify-content: space-between;">
       <appointment-demo style="margin-right: 2rem;" />
-      <collections-demo />
+      <collections-demo style="margin-right: 2rem;" />
+      <call-deflection-demo style="margin-bottom: 1.5rem;" />
     </div>
   </panel>
 </template>
 
 <script>
 import AppointmentDemo from './appointment-demo.vue'
+import CallDeflectionDemo from './call-deflection-demo.vue'
 import CollectionsDemo from './collections-demo.vue'
 
 export default {
   components: {
     AppointmentDemo,
+    CallDeflectionDemo,
     CollectionsDemo
   }
 }
