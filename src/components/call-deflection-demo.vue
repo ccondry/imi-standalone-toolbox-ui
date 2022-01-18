3dafbd644b29845a3abd269d8e5591773488e6be
Coty Condry <ccondry@cisco.com>
2022-01-18 09:10:19 -0600

    add country selection with separate phone numbers in call deflection demo

--- src/components/call-deflection-demo.vue
+++ src/components/call-deflection-demo.vue
@@ -1,12 +1,44 @@
 <template>
   <panel title="Call Deflection" aria-id="call-deflection">
+    <!-- country -->
+    <country
+    v-model="country"
+    />
     <p>
-      Call this number to start the Call Deflection Demo.
+      Call this number to start the Call Deflection Demo:
     </p>
     <p>
       <strong>
-        +1 206-848-7318
+        {{ phone }}
       </strong>
     </p>
   </panel>
 </template>
+
+<script>
+import Country from './input/country'
+
+export default {
+  components: {
+    Country
+  },
+
+  data () {
+    return {
+      country: 'US'
+    }
+  },
+
+  computed: {
+    phone () {
+      if (this.country === 'US') {
+        return '+1-206-848-7318'
+      }
+      if (this.country === 'UK') {
+        return '+44 7418 365836'
+      }
+      return ''
+    }
+  }
+}
+</script>
